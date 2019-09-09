b151195de78687f2d5ccc7874e8809973aacaf87
John Cen <john.cen@carloanscanada.com>
2019-09-09 14:16:38 -0400

    Added success and fail response methods

--- api-handler.js
+++ api-handler.js
@@ -2,6 +2,32 @@
 
 const TEST_KEY = process.env.TEST_KEY
 
+//--------------- FUNCTIONS ---------------
+
+//Returns a success response obj
+const sendOKResponse = (body) => {
+  return {
+    statusCode: 200,
+    headers: { 
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify(body)
+  }
+}
+
+//Returns an error response obj
+const sendErrorResponse = (statusCode, errorMessage) => {
+  console.error('sendErrorRepsonse: console logging error msg:\n', errorMessage)
+  return { 
+    statusCode: statusCode, 
+    headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+    body: JSON.stringify({ error: errorMessage })
+  }
+}
+
+//--------------- LAMBDA FUNCTIONS ---------------
+
 module.exports.hello = async (event) => {
   console.log("how tokens/secrets will be stored in serverless:", TEST_KEY)
 };
