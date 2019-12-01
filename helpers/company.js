a9ac85a07770e2ff5e213a6d9db8eb2524703148
John Cen <john.cen397@gmail.com>
2019-11-30 23:50:14 -0500

    Adds company helper fn left out by mistake

--- helpers/company.js
+++ helpers/company.js
@@ -17,5 +17,19 @@ const findCompanyByName = async (eventBody) => {
     }
 }
 
+const findCompanyByNameAndLocation = async (companyName, location) => {	//being used
+    try {	module.exports.findCompanyByName = findCompanyByName; 
+        let foundCompany = await db.exec(MONGO_URL, () => {	
+            return Company.find({ name: companyName, location: location })	
+        })	
+        console.log('Company Found:\n', foundCompany)	
+        return foundCompany[0]	
+    } catch (err) {	
+        console.error('caught err while trying to find Company:\n', err.message)	
+        return Status.createErrorResponse(404, "Company does not exist.")	
+    }	
+}
+
 //being used
 module.exports.findCompanyByName = findCompanyByName;
+module.exports.findCompanyByNameAndLocation = findCompanyByNameAndLocation;
